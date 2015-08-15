84fd40fb150275242c964473436c381fac5748e4
Jon Wadsworth <codescribblr@gmail.com>
2015-08-15 15:03:22 -0400

    initial commit of plugin v1.

---
+++ includes/libs/netsuite-phptoolkit/NSconfig.php
@@ -0,0 +1,9 @@
+<?php
+$nsendpoint = "2015_1";
+$nshost = "https://webservices.netsuite.com";
+
+$nsemail = "stanton@wolfpackwholesale.com";
+$nspassword = "Password300";
+$nsrole = "3";
+$nsaccount = "20816";
+?>
