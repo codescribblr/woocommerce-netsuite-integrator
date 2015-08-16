b990d220791b3c2b3351a5df57d3156336828aad
Jon Wadsworth <codescribblr@gmail.com>
2015-08-16 14:52:28 -0400

    version bump for adding cron removal to uninstall hook.

--- woocommerce-netsuite-integrator.php
+++ woocommerce-netsuite-integrator.php
@@ -5,7 +5,7 @@ Plugin URI: http://wordpress.org/plugins/woocommerce-netsuite-integrator/
 Description: WooCommerce NetSuite Integrator.
 Author: Showcase Marketing
 Author URI: http://createlaunchlead.com
-Version: 1.0.4
+Version: 1.0.5
 License: GPLv2 or later
 Text Domain: woocommerce-netsuite-integrator
 Domain Path: /languages
@@ -48,7 +48,7 @@ class SCM_WC_Netsuite_Integrator {
 	 *
 	 * @var string
 	 */
-	const VERSION = '1.0.4';
+	const VERSION = '1.0.5';
 
 	/**
 	 * Instance of this class.
